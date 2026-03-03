324a471d8c11723b00448a3473be0c3d2bd2a2d3
dstouck92 <dstouck@gmail.com>
2026-03-03 15:23:54 -0600

    Add debounce hook for Spotify search

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
